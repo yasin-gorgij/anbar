a0317bf8d69909e52759b39bdf3b57b7f4c2d951
Yasin Gorgij <yasin.gorgij@protonmail.com>
2022-12-03 09:21:36 +0330

    Add action to extract user_id

--- lib/anbar_web/controllers/inventory_controller.ex
+++ lib/anbar_web/controllers/inventory_controller.ex
@@ -4,20 +4,20 @@ defmodule AnbarWeb.InventoryController do
   alias Anbar.Inventories
   alias Anbar.Inventories.Inventory
 
-  def index(conn, _params) do
-    inventories = Inventories.list_inventories(conn.assigns.current_user.id)
+  def index(conn, _params, user_id) do
+    inventories = Inventories.list_inventories(user_id)
 
     render(conn, "index.html", inventories: inventories)
   end
 
-  def new(conn, _params) do
+  def new(conn, _params, _user_id) do
     changeset = Inventories.change_inventory(%Inventory{})
 
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"inventory" => inventory_params}) do
-    inventory_params = Map.put(inventory_params, "user_id", conn.assigns.current_user.id)
+  def create(conn, %{"inventory" => inventory_params}, user_id) do
+    inventory_params = Map.put(inventory_params, "user_id", user_id)
 
     case Inventories.create_inventory(inventory_params) do
       {:ok, inventory} ->
@@ -30,9 +30,8 @@ defmodule AnbarWeb.InventoryController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
+  def show(conn, %{"id" => id}, user_id) do
     id = String.to_integer(id)
-    user_id = conn.assigns.current_user.id
     inventory = Inventories.get_inventory!(user_id, id)
 
     items = Inventories.list_items(user_id, inventory.id)
@@ -40,18 +39,16 @@ defmodule AnbarWeb.InventoryController do
     render(conn, "show.html", inventory: inventory, items: items)
   end
 
-  def edit(conn, %{"id" => id}) do
+  def edit(conn, %{"id" => id}, user_id) do
     id = String.to_integer(id)
-    user_id = conn.assigns.current_user.id
     inventory = Inventories.get_inventory!(user_id, id)
     changeset = Inventories.change_inventory(inventory)
 
     render(conn, "edit.html", inventory: inventory, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "inventory" => inventory_params}) do
+  def update(conn, %{"id" => id, "inventory" => inventory_params}, user_id) do
     id = String.to_integer(id)
-    user_id = conn.assigns.current_user.id
     inventory = Inventories.get_inventory!(user_id, id)
 
     case Inventories.update_inventory(inventory, inventory_params) do
@@ -65,14 +62,18 @@ defmodule AnbarWeb.InventoryController do
     end
   end
 
-  def delete(conn, %{"id" => id}) do
+  def delete(conn, %{"id" => id}, user_id) do
     id = String.to_integer(id)
-    user_id = conn.assigns.current_user.id
     inventory = Inventories.get_inventory!(user_id, id)
     {:ok, _inventory} = Inventories.delete_inventory(inventory)
 
     conn
     |> put_flash(:info, "Inventory deleted successfully.")
     |> redirect(to: Routes.inventory_path(conn, :index))
+  end
+
+  def action(conn, _) do
+    args = [conn, conn.params, conn.assigns.current_user.id]
+    apply(__MODULE__, action_name(conn), args)
   end
 end
